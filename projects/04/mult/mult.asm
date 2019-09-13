096882a4a92cfb9f6b3bae9f201e1a567eef1eaf
Daisuke HIGASHI <me@lulichn.net>
2019-09-13 20:53:11 +0900

    機械語 Mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,47 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+// 結果を格納する R2 に 0 を代入
+@R2
+M=0
+
+// R1 が 0 より大きい場合は LOOP へジャンプ
+@R1
+D=M
+@LOOP
+D;JGT
+
+// ここに到達した(R1 が 0) の場合は END へジャンプ
+@END
+0;JMP
+
+(LOOP)
+    // R2 += R0
+
+    // D = R2
+    @R2
+    D=M
+    // D += R0
+    @R0
+    D=D+M
+    // R2 = D
+    @R2
+    M=D
+
+    // R1 をデクリメント
+    // D = R1
+    @R1
+    D=M
+    // D -= 1
+    D=D-1
+    // R1 = D
+    M=D
+
+    // R1 > 0 の時, Loop にジャンプ
+    @LOOP
+    D;JGT
+(END)
+    // 無限ループは Hack プログラムを "終了させる"
+    @END
+    0;JMP
