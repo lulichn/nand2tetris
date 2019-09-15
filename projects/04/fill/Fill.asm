87fcb231fcb83f996fbed3707c52861ab15814c8
Daisuke HIGASHI <me@lulichn.net>
2019-09-16 03:32:11 +0900

    機械語 Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,65 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+    // キーボード入力
+    @KBD
+    D=M
+
+    // 押下されている場合 ON へジャンプ
+    @ON
+    D;JGT
+
+    // そうでない場合 0 (000000000000) で埋める
+    @value
+    M=0
+
+    // FILL へジャンプ
+    @FILL
+    0;JMP
+
+(ON)
+    // -1 (111111111111) で埋める
+    @value
+    M=-1
+
+(FILL)
+    // カウンタ (描写位置) を初期化
+    @counter
+    M=0
+
+    (FILLLOOP)
+        @counter
+        D=M
+        
+        // アドレスを temp に確保
+        @SCREEN
+        A=A+D
+        D=A
+        @temp
+        M=D
+
+        // 描画する値
+        @value
+        D=M
+
+        // temp に描画
+        @temp
+        A=M
+        M=D
+
+        // カウンタをインクリメント
+        @counter
+        D=M+1
+        M=D
+
+        // スクリーンの最後まで
+        // 8192 = (512 / 16) * 256, 512:横, 256:縦
+        @8192
+        D=A-D
+
+        @FILLLOOP
+        D;JGT
+    
+@LOOP
+0;JMP
