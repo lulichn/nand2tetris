54ef2afa7f0226b5eb618e6890ef2b4b8c2cb703
Daisuke HIGASHI <me@lulichn.net>
2019-09-17 00:33:59 +0900

    機械語 リファクタリング

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,45 +7,31 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 
-// 結果を格納する R2 に 0 を代入
+// 結果を格納する R2 を 0 で初期化
 @R2
 M=0
 
-// R1 が 0 より大きい場合は LOOP へジャンプ
+// R1 が 0 以下の場合は END へジャンプ
 @R1
 D=M
-@LOOP
-D;JGT
-
-// ここに到達した(R1 が 0) の場合は END へジャンプ
 @END
-0;JMP
+D;JLE
 
-(LOOP)
+(LOOP)  // R2 += R0
     // R2 += R0
-
-    // D = R2
-    @R2
-    D=M
-    // D += R0
     @R0
-    D=D+M
-    // R2 = D
+    D=M
     @R2
-    M=D
+    M=M+D
 
     // R1 をデクリメント
-    // D = R1
     @R1
-    D=M
-    // D -= 1
-    D=D-1
-    // R1 = D
-    M=D
+    MD=M-1
 
     // R1 > 0 の時, Loop にジャンプ
     @LOOP
     D;JGT
+
 (END)
     // 無限ループは Hack プログラムを "終了させる"
     @END
